369897bc5733cccf75c5b0e77d0678ea22900038
Burak Yılmaz <burakyilmaz321@gmail.com>
2017-09-16 02:25:05 +0300

    basic registration

---
+++ client/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Register</h2> 
+    <input v-model="email" type="email" name="email" placeholder="email">
+    <input v-model="password" type="password" name="password" placeholder="password">
+    <button @click="register">Register</button>  
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
